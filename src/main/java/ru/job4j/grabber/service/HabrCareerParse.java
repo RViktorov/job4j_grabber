ca4968d1daf7a655c029f7f698ffe5ba9ab94ff7
Roman Viktorov <rpviktorov@gmail.com>
2026-02-28 21:36:49 +0300

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210]

--- src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -17,6 +17,7 @@ public class HabrCareerParse implements Parse { // класс отвечает �
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
 
+    private static final int PAGES_TO_PARSE = 5; //количество страниц для парсинга
     private final DateTimeParser dateTimeParser;
 
     public HabrCareerParse(DateTimeParser dateTimeParser) {
@@ -27,46 +28,47 @@ public class HabrCareerParse implements Parse { // класс отвечает �
     public List<Post> fetch() {
         var result = new ArrayList<Post>();
         try {
-            int pageNumber = 1;
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-            var connection = Jsoup.connect(fullLink);
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                //название и ссылка
-                try {
-                    var titleElement = row.select(".vacancy-card__title").first();
-                    var linkElement = titleElement.child(0);
+            for (int pageNumber = 1; pageNumber <= PAGES_TO_PARSE; pageNumber++) {
+                String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+                var connection = Jsoup.connect(fullLink);
+                var document = connection.get();
+                var rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    //название и ссылка
+                    try {
+                        var titleElement = row.select(".vacancy-card__title").first();
+                        var linkElement = titleElement.child(0);
 
-                    String vacancyName = titleElement.text();
-                    String link = String.format("%s%s", SOURCE_LINK,
-                            linkElement.attr("href"));
-                    // получаем время из вакансии
-                    var vacancyDoc = Jsoup.connect(link).get();
+                        String vacancyName = titleElement.text();
+                        String link = String.format("%s%s", SOURCE_LINK,
+                                linkElement.attr("href"));
+                        // получаем время из вакансии
+                        var vacancyDoc = Jsoup.connect(link).get();
 
-                    var timeElement = vacancyDoc.selectFirst("time");
-                    long created;
-                    if (timeElement != null) {
-                        String datetime = timeElement.attr("datetime");
-                        LocalDateTime localDateTime = dateTimeParser.parse(datetime);
+                        var timeElement = vacancyDoc.selectFirst("time");
+                        long created;
+                        if (timeElement != null) {
+                            String datetime = timeElement.attr("datetime");
+                            LocalDateTime localDateTime = dateTimeParser.parse(datetime);
 
-                        created = localDateTime
-                                .atZone(ZoneId.systemDefault()) // указываем зону
-                                .toInstant()
-                                .toEpochMilli();
-                    } else {
-                        created = System.currentTimeMillis();
+                            created = localDateTime
+                                    .atZone(ZoneId.systemDefault()) // указываем зону
+                                    .toInstant()
+                                    .toEpochMilli();
+                        } else {
+                            created = System.currentTimeMillis();
+                        }
+
+                        var post = new Post();
+                        post.setTitle(vacancyName);
+                        post.setLink(link);
+                        post.setTime(created);
+                        result.add(post);
+                    } catch (IOException e) {
+                        LOG.error("Error parsing vacancy: " + row.text(), e);
                     }
-
-                    var post = new Post();
-                    post.setTitle(vacancyName);
-                    post.setLink(link);
-                    post.setTime(created);
-                    result.add(post);
-                } catch (IOException e) {
-                    LOG.error("Error parsing vacancy: " + row.text(), e);
-                }
-            });
+                });
+            }
         } catch (IOException e) {
             LOG.error("When load page", e);
         }
